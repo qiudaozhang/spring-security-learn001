f587f8645ba74a451975471f7cd6d20eb096bc56
13928421127 <singkingcho@gmail.com>
2020-12-24 10:07:58 +0800

    Initial commit

---
+++ src/main/java/com/qiudaozhang/springsecuritylearn001/SpringSecurityLearn001Application.java
@@ -0,0 +1,14 @@
+package com.qiudaozhang.springsecuritylearn001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLearn001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityLearn001Application.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/qiudaozhang/springsecuritylearn001/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.qiudaozhang.springsecuritylearn001.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+public class SecurityConfig {
+
+
+}
